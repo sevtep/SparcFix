e91361f3eec547bab61a505348d9c4bc5b86762b
sean vuksanic <vuksan62@students.rowan.edu>
2024-04-02 13:59:11 -0400

    Sean work init

---
+++ Sean Work/Assets/Scenes/PlayerMovement.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed = 5f;
+
+    void Update() {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+        transform.Translate(moveDirection * speed * Time.deltaTime);
+    }
+}
